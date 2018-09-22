d59558196a89d6a75a881b410eaa410dc8cdf678
DucauArley <arleyducau@gmail.com>
2018-09-22 17:06:12 -0300

    Ejercicios

--- EjerciciosGuia/Ejercicio31/Negocio.cs
+++ EjerciciosGuia/Ejercicio31/Negocio.cs
@@ -18,10 +18,76 @@ namespace Ejercicio31
             caja = new PuestoAtencion(PuestoAtencion.Puesto.Caja1);
         }
 
+        public Negocio(string nombre) : this()
+        {
+            this.nombre = nombre;
+        }
          
+        public static bool operator ==(Negocio n, Cliente c)
+        {
+            bool ok = false;
+
+            for (int i = 0; i < n.clientes.Count; i++)
+            {
+                if (n.clientes.Contains(c))
+                {
+                    ok = true;
+                }
+            }
 
 
+            return ok;
+        }
 
+        public static bool operator !=(Negocio n, Cliente c)
+        {
+            return !(n == c);
+        }
+
+
+        public static bool operator +(Negocio n, Cliente c)
+        {
+            bool ok = false;
+
+            if(n != c)
+            {
+                n.clientes.Enqueue(c);
+                ok = true;
+            }
+
+            return ok;
+        }
+
+        public static bool operator ~(Negocio n)
+        {
+            bool ok = false;
+
+            if (n.caja.Atender(n.Cliente))
+            {
+                ok = true;
+            }
+            
+            return ok;
+        }
+
+
+        public Cliente Cliente
+        {
+            get
+            {
+                return this.clientes.First();
+            }
+
+            set
+            {
+                if (!this.clientes.Contains(value))
+                {
+                    this.clientes.Enqueue(value);
+                }
+            }
+
+
+        }
 
 
     }
