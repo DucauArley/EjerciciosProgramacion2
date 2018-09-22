c98faa78eff62cc334b42686f13bdf1b6dc5b7a4
DucauArley <arleyducau@gmail.com>
2018-09-22 20:40:18 -0300

    Ejercicios

---
+++ EjerciciosGuia/Ejercicio33/Libro.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio33
+{
+    class Libro
+    {
+
+        private List<string> paginas;
+
+        public Libro()
+        {
+            paginas = new List<string>();
+        }
+
+
+        public string this[int i]
+        {
+            get
+            {
+                string retorno = "";
+
+                if(i < paginas.Count)
+                {
+                    retorno = paginas.ElementAt(i);
+                }
+                
+                return retorno;
+            }
+
+            set
+            {
+                if(i > paginas.Count)
+                {
+                    paginas.Add(value);
+                }
+                else
+                {
+                    paginas.Insert(i, value);
+                }
+                
+            }
+
+        }
+
+        
+    }
+}
