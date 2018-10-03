db8631be103effea4edcbf38c5a346658bb61095
DucauArley <arleyducau@gmail.com>
2018-10-03 15:41:54 -0300

    Modelo de parcial

---
+++ ModelosDeParcial/ModeloDeParcial2/Entidades/Prestamo.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrestamosPersonales
+{
+    public enum PeriodicidadDePagos
+    {
+        Mensual,
+        Bimestral,
+        Trimestral
+    }
+
+    public enum TipoDePrestamo
+    {
+        Pesos,
+        Dolares,
+        Todos
+    }
+
+
+    public abstract class Prestamo
+    {
+        protected float monto;
+        protected DateTime vencimiento;
+
+        public Prestamo(float monto, DateTime vencimiento)
+        {
+            this.monto = monto;
+            this.vencimiento = Vencimiento;
+        }
+
+        public float Monto
+        {
+            get
+            {
+                return this.monto;
+            }
+        }
+
+        public DateTime Vencimiento
+        {
+            get
+            {
+                return this.vencimiento;
+            }
+
+            set
+            {
+                if(DateTime.Now < value)
+                {
+                    this.vencimiento = value;
+                }
+                else
+                {
+                    this.vencimiento = DateTime.Now;
+                }
+            }
+            
+        }
+
+
+        public virtual string Mostrar()
+        {
+            StringBuilder str = new StringBuilder();
+            string retorno;
+
+            str.AppendLine("Monto: " + Monto);
+            str.AppendLine("Vencimiento: " + Vencimiento);
+
+            retorno = str.ToString();
+
+            return retorno;
+        }
+
+
+        public static int OrdenarPorFecha(Prestamo p1, Prestamo p2)
+        {
+            int retorno;
+
+            if(p1.Vencimiento == p2.Vencimiento)
+            {
+                retorno = 0;
+            }
+            else
+            {
+                if(p1.Vencimiento > p2.Vencimiento)
+                {
+                    retorno = 1;
+                }
+                else
+                {
+                    retorno = -1;
+                }
+            }
+
+            return retorno;
+        }
+
+        public abstract void ExtenderPlazo(DateTime nuevoVencimiento);
+        
+    }
+}
---
+++ ModelosDeParcial/ModeloDeParcial2/Entidades/PrestamoPesos.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrestamosPersonales
+{
+    class PrestamoPesos :Prestamo
+    {
+        private float porcentajeInteres;
+
+        public PrestamoPesos(float monto, DateTime vencimiento, float intereses):base(monto, vencimiento)
+        {
+            this.porcentajeInteres = intereses;
+        }
+
+        public PrestamoPesos(Prestamo prestamo, float porcentajeIntereses):this(prestamo.Monto, prestamo.Vencimiento, porcentajeIntereses)
+        {
+        }
+
+        public float Interes
+        {
+            get
+            {
+                return CalcularIntereses();
+            }
+        }
+
+
+        private float CalcularIntereses()
+        {
+            float retorno;
+
+            retorno = (this.Monto * this.porcentajeInteres) / 100;
+
+            return retorno;
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder str = new StringBuilder();
+            string retorno;
+
+            str.AppendLine(base.Mostrar());
+            str.AppendFormat("Intereses: {0}\n", this.Interes);
+
+            retorno = str.ToString();
+
+            return retorno;
+        }
+
+        public override void ExtenderPlazo(DateTime nuevoVencimiento)
+        {
+            double dias = (nuevoVencimiento - this.Vencimiento).TotalDays;
+
+            this.porcentajeInteres = this.porcentajeInteres + (float) (dias * 0.25F);
+
+            this.Vencimiento = nuevoVencimiento;
+        }
+
+
+    }
+}
