dcd288a9f0c14ed2a7ed315f5b820dcfea79a707
DucauArley <arleyducau@gmail.com>
2018-10-19 15:17:18 -0300

    Ejercicios

--- EjerciciosGuia/Ejercicio47/Program.cs
+++ EjerciciosGuia/Ejercicio47/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Ejercicio47
 {
@@ -10,7 +11,42 @@ namespace Ejercicio47
     {
         static void Main(string[] args)
         {
+            Torneo<EquipoBasquet> t1 = new Torneo<EquipoBasquet>("NBA");
+            Torneo<EquipoFutbol> t2 = new Torneo<EquipoFutbol>("Libertadores");
+            EquipoBasquet lakers = new EquipoBasquet("Lakers", DateTime.Now);
+            EquipoBasquet spurs = new EquipoBasquet("Spurs", DateTime.Now);
+            EquipoBasquet miamiHeats = new EquipoBasquet("Miami Heats", DateTime.Now);
+            EquipoFutbol river = new EquipoFutbol("River", DateTime.Now);
+            EquipoFutbol boca = new EquipoFutbol("Boca", DateTime.Now);
+            EquipoFutbol independiente = new EquipoFutbol("Independiente", DateTime.Now);
 
+            t1 += lakers;
+            t1 += spurs;
+            t1 += miamiHeats;
+
+            t2 += river;
+            t2 += boca;
+            t2 += independiente;
+
+            Console.WriteLine(t1.Mostrar());
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(t1.JugarPartido);
+                Thread.Sleep(200);
+            }
+
+
+            Console.WriteLine(t2.Mostrar());
+            
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(t2.JugarPartido);
+                Thread.Sleep(200);
+            }
+            
+
+            Console.ReadKey();
         }
     }
 }
--- EjerciciosGuia/Ejercicio47/Torneo.cs
+++ EjerciciosGuia/Ejercicio47/Torneo.cs
@@ -11,7 +11,41 @@ namespace Ejercicio47
         private List<T> equipos;
         private string nombre;
 
-        public static bool operator ==(Torneo<T> t, Equipo e)
+        private Torneo()
+        {
+            this.equipos = new List<T>();
+        }
+        
+        public Torneo(string nombre) : this()
+        {
+            this.nombre = nombre;
+        }
+
+        public string JugarPartido
+        {
+            get
+            {
+                Random ran = new Random();
+                string retorno;
+                T e1;
+                T e2;
+
+                e1 = this.equipos.ElementAt((ran.Next(this.equipos.Count)));
+                e2 = this.equipos.ElementAt((ran.Next(this.equipos.Count)));
+
+                while(e1 == e2)
+                {
+                    e2 = this.equipos.ElementAt((ran.Next(this.equipos.Count)));
+                }
+
+                retorno = CalcularPartido(e1, e2);
+
+                return retorno;
+            }
+        }
+
+        
+        public static bool operator ==(Torneo<T> t, T e)
         {
             bool ok = false;
 
@@ -23,12 +57,12 @@ namespace Ejercicio47
             return ok;
         }
 
-        public static bool operator !=(Torneo<T> t, Equipo e)
+        public static bool operator !=(Torneo<T> t, T e)
         {
             return !(t == e);
         }
 
-        public static Torneo<T> operator +(Torneo<T> t, Equipo e)
+        public static Torneo<T> operator +(Torneo<T> t, T e)
         {
             if(t != e)
             {
@@ -37,6 +71,38 @@ namespace Ejercicio47
 
             return t;
         }
+
+        public string Mostrar()
+        {
+            StringBuilder str = new StringBuilder();
+            string retorno;
+
+            str.AppendLine("Torneo " + this.nombre);
+            str.AppendLine("Equipos participantes: ");
+
+            foreach (T e in this.equipos)
+            {
+                str.Append(e.Ficha());
+            }
+
+            retorno = str.ToString();
+
+            return retorno;
+        }
+
+        private string CalcularPartido(T e1, T e2)
+        {
+            StringBuilder str = new StringBuilder();
+            Random ran = new Random();
+            string retorno;
+
+            str.AppendLine(e1.Nombre + " " + ran.Next(40) + " - " + e2.Nombre + " " + ran.Next(40));
+
+            retorno = str.ToString();
+
+            return retorno;
+        }
+
 
 
     }
