c7eb5bc642b0334e15b0d32d89c1d9c9f177361c
DucauArley <arleyducau@gmail.com>
2018-10-25 15:04:27 -0300

    Ejercicios

---
+++ EjerciciosGuia/Ejercicio52/Boligrafo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio52
+{
+    class Boligrafo
+    {
+        private ConsoleColor colorTinta;
+        private float tinta;
+
+        public Boligrafo(int unidades, ConsoleColor color)
+        {
+            this.colorTinta = color;
+            this.tinta = unidades;
+        }
+
+
+
+
+
+    }
+}
---
+++ EjerciciosGuia/Ejercicio52/IAcciones.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio52
+{
+    interface IAcciones
+    {
+        ConsoleColor Color
+        {
+            get;
+            set;
+        }
+
+        float UnidadesDeEscritura
+        {
+            get;
+            set;
+        }
+
+
+        EscrituraWrapper Escribir(string texto);
+
+        bool Recargar(int unidades);
+
+    }
+}
