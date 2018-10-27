4f5c79ade621a49cc47327f0f0f5a739a296998e
DucauArley <arleyducau@gmail.com>
2018-10-27 20:46:59 -0300

    Ejercicios

---
+++ EjerciciosGuia/Ejercicio54/ArchivoTexto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ejercicio54
+{
+    public static class ArchivoTexto
+    {
+        public static void Guardar(string path, string texto)
+        {
+            StreamWriter archivo = new StreamWriter(path, true);
+            archivo.Write(texto);
+            archivo.Close();
+        }
+
+        public static string Leer(string path)
+        {
+            try
+            {
+                StreamReader archivo = new StreamReader(path);
+                string retorno;
+
+                retorno = archivo.ReadToEnd();
+
+                archivo.Close();
+
+                return retorno;
+            }
+            catch(FileNotFoundException e)
+            {
+                throw e;
+            }
+        }
+
+
+
+
+
+
+    }
+}
--- EjerciciosGuia/Ejercicio54/Program.cs
+++ EjerciciosGuia/Ejercicio54/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ejercicio42;
 
 namespace Ejercicio54
 {
@@ -10,6 +11,27 @@ namespace Ejercicio54
     {
         static void Main(string[] args)
         {
+            string ruta = String.Format("{0}{1}{2}-{3}{4}.txt", DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute);
+
+            try
+            {
+                OtraClase aux = new OtraClase();
+                aux.MiMetodoInstancia();  //1)Llama al metodo de la clase OtraClase...
+            }
+            catch (Exception e)
+            { //9) Aca llega DivideByZero + UnaException + MiException
+                if (!object.ReferenceEquals(e.InnerException, null))
+                {
+                    do
+                    {
+                        Console.WriteLine(e.Message);
+                        ArchivoTexto.Guardar(ruta, e.Message);
+                        e = e.InnerException;
+                    } while (!object.ReferenceEquals(e, null));
+                }
+            }
+            Console.ReadKey();
+
         }
     }
 }
